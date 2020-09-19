ea4aba101c1aa27aa1c78cddc6cad9d324b7019b
sanchitdawarsd <52976915+sanchitdawarsd@users.noreply.github.com>
2020-09-18 22:44:53 -0500

    19sep

--- src/components/Layout/FooterBar.tsx
+++ src/components/Layout/FooterBar.tsx
@@ -2,37 +2,78 @@ import Link from "next/link";
 import React from "react";
 import css from "./footer.module.scss";
 
-const NavigationBar: React.FunctionComponent = () => (
-  <div className="bg-brand-dark-foot">
-    <div>
-      <div>
-        <Link href="/collaborate">
-          <a>Collaborate</a>
-        </Link>
+export const FooterBar: React.FunctionComponent = () => (
+  <footer className={css.footer}>
+    <div className={`container ${css.content}`}>
+      <div className="row">
+        <div className="col-12 col-md-4 mb-4 mb-md-0">
+          <Link href="/">
+            <a>
+              <img className={css.logo} src="/static/images/opencertslogo.png" alt="WorldCerts logo" />
+            </a>
+          </Link>
+        </div>
+        <div className="col-12 col-md col-lg-2 mb-4 mb-md-0">
+          <h6>Partners</h6>
+          <ul className="reset-list">
+            <li>
+              <Link href="/registry">
+                <a>Registry</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/collaborate">
+                <a>Collaborate</a>
+              </Link>
+            </li>
+            <li>
+              <a href="https://go.gov.sg/opencerts-feedback" target="_blank" rel="noopener noreferrer">
+                Feedback
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 col-md col-lg-2 mb-4 mb-md-0">
+          <h6>Support</h6>
+          <ul className="reset-list">
+            <li>
+              <Link href="/faq">
+                <a>FAQ</a>
+              </Link>
+            </li>
+            <li>
+              <a href="https://github.com/OpenCerts" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 col-md col-lg-2 mb-4 mb-md-0">
+          <h6>Legal</h6>
+          <ul className="reset-list">
+            <li>
+              <Link href="/privacy">
+                <a>Privacy Policy</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/terms">
+                <a>Terms of Use</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <Link href="/faq">
-          <a>FAQ</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/registry">
-          <a>Registry</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/privacy">
-          <a>Privacy Policy</a>
-        </Link>
-      </div>
-      <div>
-        <Link href="/terms">
-          <a>Terms of Use</a>
-        </Link>
-      </div>
-      <div></div>
     </div>
-  </div>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-12">
+          <hr />
+          <p>Copyright © 2020 Worldcerts</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
 
-export default NavigationBar;
+export default FooterBar;
